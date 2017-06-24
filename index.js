ba447bc2f71f9b3d43286ad84f46bdddac2ca51f
Simon.H <835374624@qq.com>
2017-06-24 17:50:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,70 +6,102 @@ function getPosition(str, m, i) {
   return str.split(m, i).join(m).length;
 }
 
-hexo.extend.filter.register('after_post_render', function(data){
+hexo.extend.filter.register('after_post_render',
+function(data) {
   var config = hexo.config;
-  if(config.post_asset_folder){
+  if (config.post_asset_folder) {
     var link = data.permalink;
-	var beginPos = getPosition(link, '/', 3) + 1;
-	// In hexo 3.1.1, the permalink of "about" page is like ".../about/index.html".
-	var endPos = link.lastIndexOf('/') + 1;
+    var beginPos = getPosition(link, '/', 3) + 1;
+    // In hexo 3.1.1, the permalink of "about" page is like ".../about/index.html".
+    var endPos = link.lastIndexOf('/') + 1;
     link = link.substring(beginPos, endPos);
 
     var toprocess = ['excerpt', 'more', 'content'];
-    for(var i = 0; i < toprocess.length; i++){
+    for (var i = 0; i < toprocess.length; i++) {
       var key = toprocess[i];
- 
+
       var $ = cheerio.load(data[key], {
         ignoreWhitespace: false,
         xmlMode: false,
         lowerCaseTags: false,
         decodeEntities: false
       });
-      $('img').each(function(){
-    // For windows style path, we replace '\' to '/'.
-        if(!$(this).attr('src')) {
+      $('img').each(function() {
+        // For windows style path, we replace '\' to '/'.
+        if (!$(this).attr('src')) {
           return;
         }
         var src = $(this).attr('src').replace('\\', '/');
-        console.log(link);
-        console.log(src);
-        if(!/http[s]*.*|\/\/.*/.test(src)){
-		  // For "about" page, the first part of "src" can't be removed.
-		  // In addition, to support multi-level local directory.
-		  var linkArray = link.split('/').filter(function(elem){
-		    return elem != '';
-		  });
-		  var srcArray = src.split('/').filter(function(elem){
-		    return elem != '';
-		  });
-		  if(linkArray[linkArray.length - 1] == srcArray[0])
-		    srcArray.shift();
-          src = srcArray.join('/');
-          $(this).attr('src', '/' + link + src);
-          console.info("update link as:-->"+link + src);
+        //console.log(link);
+        //console.log(src);
+        if (!/http[s]*.*|\/\/.*/.test(src)) {
+          for (var j = 0; j < config.assetimage.images.length; j++) {
+            if (src.toLowerCase().endsWith(config.assetimage.images[j])) {
+              // For "about" page, the first part of "src" can't be removed.
+              // In addition, to support multi-level local directory.
+              var linkArray = link.split('/').filter(function(elem) {
+                return elem != '';
+              });
+              var srcArray = src.split('/').filter(function(elem) {
+                return elem != '';
+              });
+              if (linkArray[linkArray.length - 1] == srcArray[0]) srcArray.shift();
+              src = srcArray.join('/');
+              if (config.assetimage.useqiniu) {
+                $(this).attr('src', '//' + config.assetimage.qiniuurl + '/' + link + src);
+                console.info('update img src as:-->' + '//' + config.assetimage.qiniuurl + '/' + link + src);
+              } else {
+                $(this).attr('src', '/' + link + src);
+                console.info('update img src as:-->' + '/' + link + src);
+              }
+              break;
+            }
+          }
         }
       });
-      $('a').each(function(){
-    // For windows style path, we replace '\' to '/'.
-        if(!$(this).attr('href')) {
+      $('a').each(function() {
+        // For windows style path, we replace '\' to '/'.
+        if (!$(this).attr('href')) {
           return;
         }
         var src = $(this).attr('href').replace('\\', '/');
-        console.log(link);
-        console.log(src);
-        if(!/http[s]*.*|\/\/.*/.test(src)){
-		  // For "about" page, the first part of "src" can't be removed.
-		  // In addition, to support multi-level local directory.
-		  var linkArray = link.split('/').filter(function(elem){
-		    return elem != '';
-		  });
-		  var srcArray = src.split('/').filter(function(elem){
-		    return elem != '';
-		  });
-		  if(linkArray[linkArray.length - 1] == srcArray[0])
-		    srcArray.shift();
-          src = srcArray.join('/');
-          $(this).attr('href', '/' + link + src);
+        //console.log(link);
+        //console.log(src);
+        if (!/http[s]*.*|\/\/.*/.test(src)) {
+          var needUpdate = false;
+          for (var j = 0; j < config.assetimage.images.length; j++) {
+            if (src.toLowerCase().endsWith(config.assetimage.images[j])) {
+              needUpdate = true;
+              break;
+            }
+          }
+          if (!needUpdate) {
+            for (var j = 0; j < config.assetimage.attachment.length; j++) {
+              if (src.toLowerCase().endsWith(config.assetimage.attachment[j])) {
+                needUpdate = true;
+                break;
+              }
+            }
+          }
+          if (needUpdate) {
+            // For "about" page, the first part of "src" can't be removed.
+            // In addition, to support multi-level local directory.
+            var linkArray = link.split('/').filter(function(elem) {
+              return elem != '';
+            });
+            var srcArray = src.split('/').filter(function(elem) {
+              return elem != '';
+            });
+            if (linkArray[linkArray.length - 1] == srcArray[0]) srcArray.shift();
+            src = srcArray.join('/');
+            if (config.assetimage.useqiniu) {
+              $(this).attr('href', '//' + config.assetimage.qiniuurl + '/' + link + src);
+              console.info('update a href as:-->' + '//' + config.assetimage.qiniuurl + '/' + link + src);
+            } else {
+              $(this).attr('href', '/' + link + src);
+              console.info('update a href as:-->' + '/' + link + src);
+            }
+          }
         }
       });
       data[key] = $.html();
